92fa06e1a1d508cead862c34b019ef80222d7e50
Graham <mogmog@gmail.com>
2018-08-17 10:34:31 +0100

    adding video

--- src/components/Cards/InstagramCard/InstagramCard.js
+++ src/components/Cards/InstagramCard/InstagramCard.js
@@ -22,13 +22,15 @@ class InstagramCard extends Component {
 
       <div>
 
-        <Card style={{backgroundColor: 'rgba(255,255,255,0.7)' }}>
+        <Card style={{backgroundColor: 'rgba(255,255,255,0.8)' }}>
 
           <Card.Body style={{'paddingTop' : '20px', height : '57vh', 'textAlign' :'center'}}>
 
             { !data.instagram && <Button onClick={pageActions.addInstagram }> Add Instagram </Button> }
 
-            { data.instagram && <img style={{ opacity : 0.8, width : '96%', height : 'auto'}} alt="something" src={data.instagram.images.standard_resolution.url} />}
+            { data.instagram && data.instagram.type === 'image' && <img style={{ opacity : 0.85, width : '96%', height : 'auto'}} alt="something" src={data.instagram.images.standard_resolution.url} />}
+
+            { data.instagram && data.instagram.type === 'video' &&  <video width="96%" height="auto" controls> <source src={data.instagram.videos.standard_resolution.url} type="video/mp4"/> </video>}
 
             <div style={{height : '10%' }}>
               <CustomEditor isAdmin={isAdmin} index={index} onTextChange={pageActions.updateText} data={editorState} />
